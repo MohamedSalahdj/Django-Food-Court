a102d344b65a7ce6e1f3f5a626534f4db6c64208
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-09-01 07:58:14 +0300

    initial product app with, urls

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    # Project-app
+    'product.apps.ProductConfig', 
+    
 ]
 
 MIDDLEWARE = [
