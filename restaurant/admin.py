51244078e5fd237c04fad93de7442ac0c55589b6
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-11-02 07:48:04 +0200

    added, Review model to django adminstraion

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Restaurant, Category, Dish
+from .models import Restaurant, Category, Dish, Review
 
 admin.site.register(Restaurant)
 admin.site.register(Category)
 admin.site.register(Dish)
+admin.site.register(Review)
